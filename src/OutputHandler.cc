7fe5c3d25fd8e3b2543dd348c250bb02226bb9b6
simonge <simon.gardner@glasgow.ac.uk>
2026-01-20 21:50:45 +0000

    change files to edm4hep.root

--- src/OutputHandler.cc
+++ src/OutputHandler.cc
@@ -3,12 +3,8 @@
 #include <stdexcept>
 
 std::unique_ptr<OutputHandler> OutputHandler::create(const std::string& filename) {
-    // Get file extension
-    size_t last_dot = filename.find_last_of(".");
-    std::string extension = (last_dot != std::string::npos) ? filename.substr(last_dot + 1) : "";
-    
-    // Create appropriate handler based on extension
-    if (extension == "root") {
+    // Check if filename includes .edm4hep.root
+    if (filename.find(".edm4hep.root") != std::string::npos) {
         return std::make_unique<EDM4hepOutputHandler>();
     }
     
@@ -16,7 +12,7 @@ std::unique_ptr<OutputHandler> OutputHandler::create(const std::string& filename
     throw std::runtime_error(
         "Unsupported output format: " + filename + "\n"
         "Currently supported formats:\n"
-        "  - *.root (EDM4hep format)\n"
+        "  - Files containing '.edm4hep.root' (e.g., output.edm4hep.root)\n"
         "Planned formats:\n"
         "  - *.hepmc3 (HepMC3 format)"
     );
